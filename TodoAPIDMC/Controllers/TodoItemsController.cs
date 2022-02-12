87de23af6130454b742c5eedf0b911a256de305a
amiokuefe1 <amiokuefe@gmail.com>
2022-02-12 16:09:35 +0100

    Add project files.

---
+++ TodoAPIDMC/Controllers/TodoItemsController.cs
@@ -0,0 +1,110 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoAPIDMC.Models;
+
+namespace TodoAPIDMC.Controllers // this is the overall name of the controller class for this project
+{
+    [Route("api/[controller]")]// this is set in our launch settings as the global controller url path for each REST API request/response
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoItemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        [HttpGet]//does adding this attribute takes care of adding the complete route attached to this method in the controller?
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();// dbName.dbModel.dbQuery
+        }
+
+        // GET: api/TodoItems/5
+        [HttpGet("{id}")] // fetches the route to which this controller will be executed
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id) // the name of the controller method that is executed on the specified url
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // PUT: api/TodoItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")] // this attribute helps specify to the api controller that 
+        //this is a put method and hopefully imports security functionalities in-built to our put method
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified; // what does this mean?
+
+            try
+            {
+                await _context.SaveChangesAsync(); // execute put request into the db asychronously
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoItemExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TodoAPIDMC/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TodoAPIDMC.Models // this is model directory associated with our DB
+{
+    public class TodoContext : DbContext // creating our DB class derived from DbContext (Inheritance)
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) // creating the provision of accepting options in the constructor of our DB Instance
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!; // instantiating the available model class in our model directory as field in our DB Class
+    }
+}
